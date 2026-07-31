0bac900051a7d4b80cdb29d07b54f57cfa334668
somlosocram-star <somlosocram@gmail.com>
2026-08-01 01:51:14 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
-const CACHE='elmaster-v1';
-const ASSETS=['./','./index.html','./manifest.json','./img/domina.jpg','./img/elegante.jpg','./img/oscuro.jpg','./img/caballero.jpg'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+const CACHE='elmaster-v2';
+const IMGS=['domina.jpg','elegante.jpg','oscuro.jpg','caballero.jpg'];
+const ASSETS=['./','./index.html','./manifest.json',
+  ...IMGS.map(f=>'./img/'+f),...IMGS.map(f=>'./'+f)];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>Promise.allSettled(ASSETS.map(a=>c.add(a)))));
+  self.skipWaiting();
+});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request)));});
