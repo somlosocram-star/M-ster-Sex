21a0ebbff551fd952b455bd5fad34e7b92d50da7
somlosocram-star <somlosocram@gmail.com>
2026-08-01 07:23:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='elmaster-v6';
-const IMGS=['domina.jpg','elegante.jpg','oscuro.jpg','caballero.jpg'];
+const CACHE='elmaster-v8';
+const IMGS=['domina.jpg','elegante.jpg','oscuro.jpg','caballero.jpg','icon-192.png','icon-512.png'];
 const ASSETS=['./','./index.html','./manifest.json',
   ...IMGS.map(f=>'./img/'+f),...IMGS.map(f=>'./'+f)];
 self.addEventListener('install',e=>{
